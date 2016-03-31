99d1395638aa7ce3c2f379e1abd513e04de303e0
crossbot <crossbot80@gmail.com>
2016-04-01 04:56:13 +0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {89764429},--Sudo users
+    sudo_users = {190698588,89764429},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
